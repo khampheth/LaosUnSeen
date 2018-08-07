12e829869ac3d6e1735d1618be9ddf266e532761
khampheth <khampheth.b@gmail.com>
2018-08-07 15:09:42 +0700

    ToolBar Success

--- app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/MainFragment.java
@@ -1,16 +1,40 @@
 package masterung.androidthai.in.th.laosunseen.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+        import android.os.Bundle;
+        import android.support.annotation.NonNull;
+        import android.support.annotation.Nullable;
+        import android.support.v4.app.Fragment;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.TextView;
 
-import masterung.androidthai.in.th.laosunseen.R;
+        import masterung.androidthai.in.th.laosunseen.R;
 
 public class MainFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+     //   Register Controller
+        TextView textView = getView().findViewById(R.id.txtRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+//              replace fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+
+            }
+        });
+
+    }   //Method main
 
     @Nullable
     @Override
---
+++ app/src/main/java/masterung/androidthai/in/th/laosunseen/fragment/RegisterFragment.java
@@ -0,0 +1,45 @@
+package masterung.androidthai.in.th.laosunseen.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import masterung.androidthai.in.th.laosunseen.MainActivity;
+import masterung.androidthai.in.th.laosunseen.R;
+
+public class RegisterFragment extends Fragment{
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        create toolbar
+        Toolbar toolbar = getView().findViewById(R.id.toolBarRegister);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+        ((MainActivity)getActivity()).getSupportActionBar().setTitle("Register");
+        ((MainActivity)getActivity()).getSupportActionBar().setSubtitle("Please chose all field");
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+
+    }   //main class
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_register,container,false);
+        return view;
+    }
+}
